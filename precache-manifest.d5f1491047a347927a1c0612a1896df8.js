14ef93da8924faf64351a85ca35f17fc55971c32
Minh Le <minh.le@construct.sg>
2019-01-31 13:57:06 +0700

    Updates

---
+++ precache-manifest.d5f1491047a347927a1c0612a1896df8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64d99b9692104705a817",
+    "url": "/nfq/static/css/main.6d2401b7.chunk.css"
+  },
+  {
+    "revision": "64d99b9692104705a817",
+    "url": "/nfq/static/js/main.64d99b96.chunk.js"
+  },
+  {
+    "revision": "ba1f6ddc1fbe66eaef3f",
+    "url": "/nfq/static/js/1.ba1f6ddc.chunk.js"
+  },
+  {
+    "revision": "7bcfaadcfdd74f17276f",
+    "url": "/nfq/static/js/runtime~main.7bcfaadc.js"
+  },
+  {
+    "revision": "f5b6bb69bfc57d2123ffe883c9b71998",
+    "url": "/nfq/index.html"
+  }
+];
